8b64bbd43398bf2630a1016041d7d7c0bdc29fd2
Shelter <pradosh.balaji@gmail.com>
2023-04-27 00:25:09 +0530

    Modified date formatting

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,13 +3,25 @@ import "./Home.css";
 
 function Home({ data }) {
   const incident = data;
+  function formatDate(date) {
+    let formattedDate = new Date(date);
+    if (date == "") return "";
+  
+    return (
+      formattedDate.getDate().toString().padStart(2, 0) +
+      "/" +
+      (formattedDate.getMonth() + 1).toString().padStart(2, 0) +
+      "/" +
+      formattedDate.getFullYear()+'  '+formattedDate.getHours()+":"+formattedDate.getMinutes() +":" + formattedDate.getSeconds().toString().padStart(2, '0')
+    );
+  }
   const rows = incident.map((el) => (
     <tr key={el._id}>
       <td>{el._id}</td>
       <td style={{textTransform:"uppercase"}}>{el.type}</td>
       <td style={{textTransform:"uppercase"}}>{el.levelOfAlert}</td>
       <td>{el.locality}</td>
-      <td>{el.time}</td>
+      <td>{formatDate(el.time)}</td>
     </tr>
   ));
 
